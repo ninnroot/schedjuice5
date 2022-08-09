6381504a6bad7ad1566a7605ec97ee32bfd64848
ninnroot <misterjames.thiha@gmail.com>
2022-08-09 17:09:44 +0800

    -

--- app_users/models.py
+++ app_users/models.py
@@ -3,6 +3,18 @@ from schedjuice5.models import BaseModel
 from app_auth.models import Account
 
 
-class Staff(BaseModel):
+class BaseUser(BaseModel):
+    username = models.CharField(max_length=256)
     name = models.CharField(max_length=256)
+    about = models.TextField()
+    dob = models.DateField()
+    secondary_email = models.EmailField()
+    gender = models.CharField(max_length=128)
+
+    class Meta:
+        abstract = True
+
+
+class Staff(BaseUser):
+
     account = models.ForeignKey(Account, on_delete=models.CASCADE)
