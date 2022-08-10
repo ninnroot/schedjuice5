021462a03e9495c636486e3a75d61e196cedacc1
Guru Hein <to.72.72.78.111@gmail.com>
2022-08-10 11:20:00 +0630

    change seeding.py

--- schedjuice5/management/commands/load-fake-data.py
+++ schedjuice5/management/commands/load-fake-data.py
@@ -1,5 +1,6 @@
 from django.core.management.base import BaseCommand, CommandError
-from schedjuice5.seedings import addEntity, dyamicAddEntity, get_field_value
+from django.core.management import call_command
+from schedjuice5.seedings import addEntity, dyamicAddEntity
 from app_users.models import *
 from app_finance.models import *
 from app_auth.models import *
@@ -9,7 +10,8 @@ class Command(BaseCommand):
     help = "Help to seed the database"
 
     def handle(self, *args, **options):
-        get_field_value()
-        # dyamicAddEntity(Account, 3)
-        # dyamicAddEntity(Staff, 10, account=Account.objects.all())
-        # dyamicAddEntity(Record, 3, bank_type=["KBZ", "AYA", "CB", "KBZ_Pay"])
+        call_command("flush")
+        call_command("seed", "app_auth", "--number=10", verbosity=0)
+        call_command("seed", "app_docs", "--number=10", verbosity=0)
+        call_command("seed", "app_finance", "--number=10", verbosity=0)
+        call_command("seed", "app_users", "--number=10", verbosity=0)
--- schedjuice5/seedings.py
+++ schedjuice5/seedings.py
@@ -3,7 +3,7 @@ from app_users.models import *
 from app_finance.models import *
 from app_auth.models import *
 import random
-from django.apps import apps
+import datetime
 
 seeder = Seed.seeder()
 
@@ -43,71 +43,54 @@ def addEntity():
     return inserted_pks
 
 
-def get_field_value():
-    registered_apps = ["app_users", "app_docs", "app_finance", "app_auth"]
-    models = []
-    for i in registered_apps:
-        models += [i for i in apps.get_app_config(i).get_models()]
-
-    for i in models:
-        fields = {}
-        for j in i._meta.get_fields():
-            if j.name == "id":
-                continue
-            elif j.name == "name":
-                fields[j.name] = seeder.faker.name()
-            elif j.name in ["email", "secondary_email"]:
-                fields[j.name] = seeder.faker.email()
-            elif j.name == "password":
-                fields[j.name] = seeder.faker.password()
-            elif j.get_internal_type() == "BooleanField":
-                fields[j.name] = random.choice([True, False])
-            elif j.get_internal_type() == "DateTimeField":
-                fields[j.name] = seeder.faker.date_time_this_year()
-            elif j.get_internal_type() == "IntegerField":
-                fields[j.name] = random.randint(1, 100)
-            elif j.get_internal_type() == "CharField":
-                fields[j.name] = seeder.faker.lorem()
-
-        seeder.add_entity(i, 100, fields)
-
-        inserted_pks = seeder.execute()
-        return inserted_pks
-
-
 def dyamicAddEntity(model, count, **kwargs):
-    field_names = [field.name for field in model._meta.get_fields()] + [
-        "created_at",
-        "updated_at",
-    ]
-    if kwargs is not None:
-        for key in kwargs:
-            if key not in field_names:
-                raise Exception(f"{key} is not a valid field name for {model}")
+    fields = [
+                (field.name, field.get_internal_type()) 
+                    for field in model._meta.get_fields() 
+                        if field.name not in ["id", "last_login"] and field.get_internal_type() not in ["ForeignKey", "ManyToManyField", "OneToOneField"]
+             ]
 
-    def get_value(field_name):
-        if field_name in kwargs:
-            return random.choice(kwargs[field_name])
-        elif field_name == "name":
+    for field in fields:
+        print(field)
+
+    print(kwargs)
+
+
+    def get_value(name, field_type):
+        if name in kwargs:
+            return random.choice(kwargs[name])
+        elif name == "name":
             return seeder.faker.name()
-        elif field_name == "email":
+        elif name == "email":
             return seeder.faker.email()
-        elif field_name == "password":
+        elif name == "password":
             return seeder.faker.password()
-        elif field_name == "is_active":
+        elif name == "is_active":
             return True
-        elif field_name == "is_staff":
+        elif name == "is_staff":
             return False
-        elif field_name == "is_superuser":
+        elif name == "is_superuser":
             return False
-        elif field_name == "created_at" or field_name == "updated_at":
-            return seeder.faker.date_time_this_year()
-        elif field_name == "id":
+        elif name == "id":
             return None
         else:
-            return seeder.faker.name()
+            if field_type == "BooleanField":
+                return random.choice([True, False])
+            elif field_type == "IntegerField":
+                return random.randint(1, 100)
+            elif field_type == "DateTimeField":
+                return f"{seeder.faker.date_time_this_year()}"
+            elif field_type == "CharField":
+                return "abcabc"
+            else:
+                raise Exception(f"{name} is not a valid field name for {model}")
 
-    seeder.add_entity(model, count, {field: get_value(field) for field in field_names})
+    seeder.add_entity(
+        model, count, 
+        {
+            field[0]: lambda x: get_value(field[0], field[1]) for field in fields
+        }
+    )
 
     inserted_pks = seeder.execute()
     return inserted_pks
