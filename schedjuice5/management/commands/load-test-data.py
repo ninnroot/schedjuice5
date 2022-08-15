30ae1331660c96cc5d76617affa48ed5e9623274
Guru Hein <to.72.72.78.111@gmail.com>
2022-08-15 16:27:29 +0630

    prepare fake data for app_users

---
+++ schedjuice5/management/commands/load-test-data.py
@@ -0,0 +1,41 @@
+from django.core.management import BaseCommand, call_command
+
+import csv
+
+from app_auth.models import *
+from app_users.models import *
+from app_management.models import *
+from app_utils.models import *
+from app_finance.models import *
+from app_docs.models import *
+from app_campus.models import *
+from app_course.models import *
+
+class Command(BaseCommand):
+    
+    models = (
+        (Account, 'accounts.csv'),
+        (Staff, 'staffs.csv'),
+        (Guardian, 'guardians.csv'),
+        (Student, 'students.csv'),
+        (BankAccount, 'bank-accounts.csv'), # parent
+        (StaffBankAccount, 'staff-bank-accounts.csv'), # child of BankAccount
+        (StudentBankAccount, 'student-bank-accounts.csv'), # child of BankAccount
+        (Address, 'addresses.csv'), # parent
+        (StaffAddress, 'staff-addresses.csv'), # child of Address
+        (StudentAddress, 'student-addresses.csv'), # child of Address
+    )
+
+    def load_data(self, model, filename):
+        with open(f"schedjuice5/data/{filename}", 'r') as f:
+            reader = csv.DictReader(f, delimiter=',')
+            for row in reader:
+                model.objects.create(**row)
+            print(f"{filename} is loaded")
+
+    def handle(self, *args, **options):
+        call_command('flush', '--verbosity=0')
+        for model in self.models:
+            self.load_data(model[0], model[1])
+        
+        print('Done')
