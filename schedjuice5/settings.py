b90a1ea9b7406c19f294c0c0f8405827d75cf6c5
ninnroot <misterjames.thiha@gmail.com>
2022-09-01 19:08:54 +0800

    added allowed hosts

--- schedjuice5/settings.py
+++ schedjuice5/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config("DJANGO_SECRET_KEY", cast=str)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config("DEBUG", cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["127.0.0.1", "54.255.129.227"]
 
 
 # Application definition
