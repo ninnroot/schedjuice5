54bfeab722371a026b05aeac7947d267ecdeffd0
Guru Hein <to.72.72.78.111@gmail.com>
2022-09-12 11:33:28 +0630

    updated swagger ui schema

---
+++ schedjuice5/swagger_serializers.py
@@ -0,0 +1,10 @@
+from importlib.metadata import requires
+from rest_framework import serializers
+
+class FilterSerializer(serializers.Serializer):
+    field_name = serializers.CharField(max_length=25)
+    operator = serializers.CharField(max_length=25)
+    value = serializers.CharField(max_length=25)
+
+class FilterParamsSerializer(serializers.Serializer):
+    filter_params = FilterSerializer(many=True)
