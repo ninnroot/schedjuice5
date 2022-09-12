ad2287e0ff9f769716d0b8d32bf6b468d701e6cf
Guru Hein <to.72.72.78.111@gmail.com>
2022-09-12 21:03:48 +0630

    added cors allowed origins

--- schedjuice5/settings.py
+++ schedjuice5/settings.py
@@ -170,8 +170,11 @@ REST_FRAMEWORK = {
 
 
 # CORS
-
-CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+# CORS_ALLOW_ALL_ORIGINS = True
 
 
 # JWT
