d1b506ca43592403e7d50000d229be96dfb29785
Guru Hein <to.72.72.78.111@gmail.com>
2022-09-13 23:43:08 +0630

    added total_pages to response

--- schedjuice5/pagination.py
+++ schedjuice5/pagination.py
@@ -1,6 +1,8 @@
 from rest_framework.pagination import PageNumberPagination
 from rest_framework.response import Response
 
+import math
+
 
 # customizing the PageNumberPagination class to my liking.
 
@@ -13,6 +15,9 @@ class CustomPagination(PageNumberPagination):
     def get_count_per_page(self):
         return len(list(self.page))
 
+    def get_total_pages(self):
+        return math.ceil(self.page.paginator.count / self.get_count_per_page())
+
     def get_paginated_response(self, *args, **kwargs):
         return {
             "links": {
@@ -20,5 +25,6 @@ class CustomPagination(PageNumberPagination):
                 "previous": self.get_previous_link(),
             },
             "count": self.page.paginator.count,
-            "count_per_page": self.get_count_per_page()
+            "count_per_page": self.get_count_per_page(),
+            "total_pages": self.get_total_pages()
         }
