6f3300ba951fba089482b3220687cfc3f865b6cb
Guru Hein <to.72.72.78.111@gmail.com>
2023-01-16 23:34:49 +0800

    added error_message and updated app_assignments models

--- app_assignment/models.py
+++ app_assignment/models.py
@@ -1,9 +1,10 @@
 from django.db import models
 from app_users.models import Staff, Student
 from app_course.models import Course
+from schedjuice5.models import BaseModel
 from schedjuice5.validators import *
 
-class Assignment(models.Model):
+class Assignment(BaseModel):
     name = models.CharField(max_length=256, validators=[englishAndSomeSpecialValidation])
     instruction = models.TextField()
     start_date = models.DateTimeField()
@@ -15,18 +16,18 @@ class Assignment(models.Model):
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
         
 
-class Attachment(models.Model):
+class Attachment(BaseModel):
     attached_file = models.FileField(upload_to="assignments/questions")
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
     
 
-class Submission(models.Model):
+class Submission(BaseModel):
     description = models.TextField()
     student = models.ForeignKey(Student, on_delete=models.CASCADE)
     assignment = models.ForeignKey(Assignment, on_delete=models.CASCADE)
 
 
-class SubmissionAttachment(models.Model):
+class SubmissionAttachment(BaseModel):
     attached_file = models.FileField(upload_to="assignments/answers")
     submission = models.ForeignKey(Submission, on_delete=models.CASCADE)
 
--- schedjuice5/error_messages_format.py
+++ schedjuice5/error_messages_format.py
@@ -4,5 +4,7 @@ error_messages = {
     'invalid': 'devs:A valid integer is required.|users:blah blah....', 
     'max_value': 'devs:Ensure this value is less than or equal to {max_value}.|users:blah blah....', 
     'min_value': 'devs:Ensure this value is greater than or equal to {min_value}.|users:blah blah....', 
-    'max_string_length': 'devs:String value too large.|users:blah blah....'
+    'max_string_length': 'devs:String value too large.|users:blah blah....',
+    'does_not_exist': 'devs:PrimaryKeyRelatedField does not exist.|users: blah blah...',
+    'invalid_choice': 'devs: Wrong Choice.|users: blah blah...'
 }
