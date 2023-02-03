450ea327d6bad01f5d5cfbd1622d502390dbc9c2
ninnroot <misterjames.thiha@gmail.com>
2023-02-03 16:48:42 +0800

    get available staff for a course function done

--- app_availability/views.py
+++ app_availability/views.py
@@ -1,8 +1,9 @@
+from django.core.exceptions import BadRequest
 from django.db.models import Q
 from rest_framework.views import Request
 
-from app_course.models import Event
-from app_course.serializers import EventSerializer
+from app_course.models import Course, Event
+from app_course.serializers import CourseSerializer, EventSerializer
 from app_users.models import Staff
 from app_users.serializers import StaffSerializer
 from schedjuice5.views import BaseView
@@ -12,6 +13,7 @@ class AvailabilityView(BaseView):
     name = "list of availabilities"
 
     def get(self, request: Request, model: str, obj_id: int):
+
         model = model.lower()
         if model == "staff":
             staff = Staff.objects.filter(pk=obj_id).first()
@@ -24,21 +26,32 @@ class AvailabilityView(BaseView):
 
             self.serializer = EventSerializer
             self.model = Event
-
-            # the staff's existing assigned events
+            try:
+                self.sorts = self.get_sort_param(request)
+                self.expand = self.get_expand_param(request)
+                self.fields = self.get_field_filter_param(request)
+            except BadRequest as e:
+                return self.send_response(
+                    True, "bad_request", {"details": str(e)}, status=400
+                )
+            # The staff's existing assigned events
             assigned_events = staff.staffs_events.prefetch_related("event").all()
 
-            # from event table, get events which date are in the 'assigned_events' set
+            # From event table, get events of which dates are in the 'assigned_events' set
             same_day_events = (
                 Event.objects.filter(date__in=[i.event.date for i in assigned_events])
                 .exclude(id__in=[i.event.id for i in assigned_events])
                 .all()
             )
 
-            # the timeslots (events) that the staff is not free. Obviously, they will include the staff's original assigned events
+            # The timeslots (events) that the staff is not free for.
+            # Obviously, they will include the staff's original assigned events
             not_free = [i.event.id for i in assigned_events]
             for i in assigned_events:
-                # iterate through each day that the staff has events on. Then, check if the events are colliding. If yes, then, append to 'not_free' set.
+                # Iterate through each day that the staff has events on,
+                # get events of the same date from 'same_day_events' set.
+                # Then, check if the events are colliding.
+                # If yes, append to 'not_free' set.
                 not_free.extend(
                     [
                         x.id
@@ -52,21 +65,136 @@ class AvailabilityView(BaseView):
                 )
 
             # 'free_events' set is just the compliment of the 'not_free' set.
-            free_events = Event.objects.exclude(id__in=not_free).all()
+            free_events = (
+                Event.objects.exclude(id__in=not_free).all().order_by(*self.sorts)
+            )
 
             serialized_data = self.get_serializer(
-                self.paginate_queryset(free_events, request), many=True
+                self.paginate_queryset(free_events, request),
+                many=True,
+                expand=self.expand,
+                fields=self.fields,
             )
             return self.send_response(
                 False,
                 "all_good",
-                {**self.get_paginated_response(), "events": serialized_data.data},
+                {**self.get_paginated_response(), "data": serialized_data.data},
             )
 
         elif model == "event":
-            print("event")
+            event = Event.objects.filter(pk=obj_id).first()
+            if event is None:
+                return self.send_response(
+                    True,
+                    "not_found",
+                    {"details": f"Event with id {obj_id} does not exist."},
+                )
+
+            self.serializer = StaffSerializer
+            self.model = Staff
+            try:
+                self.sorts = self.get_sort_param(request)
+                self.expand = self.get_expand_param(request)
+                self.fields = self.get_field_filter_param(request)
+            except BadRequest as e:
+                return self.send_response(
+                    True, "bad_request", {"details": str(e)}, status=400
+                )
+
+            colliding_events = (
+                Event.objects.filter(date=event.date)
+                .exclude(
+                    (Q(time_from__gte=event.time_to) or Q(time_to__lte=event.time_from))
+                )
+                .all()
+            )
+            free_staffs = (
+                Staff.objects.exclude(
+                    staffs_events__event__id__in=[i.id for i in colliding_events]
+                )
+                .prefetch_related("staffs_events__event")
+                .all()
+            )
+            serialized_data = self.get_serializer(
+                self.paginate_queryset(free_staffs, request),
+                many=True,
+                expand=self.expand,
+                fields=self.fields,
+            )
+            return self.send_response(
+                False,
+                "all_good",
+                {**self.get_paginated_response(), "data": serialized_data.data},
+            )
+
         elif model == "course":
-            print("course")
+            course = Course.objects.filter(pk=obj_id).first()
+            if course is None:
+                return self.send_response(
+                    True,
+                    "not_found",
+                    {"details": f"Course with id {obj_id} does not exist."},
+                )
+
+            self.serializer = StaffSerializer
+            self.model = Staff
+            try:
+                self.sorts = self.get_sort_param(request)
+                self.expand = self.get_expand_param(request)
+                self.fields = self.get_field_filter_param(request)
+            except BadRequest as e:
+                return self.send_response(
+                    True, "bad_request", {"details": str(e)}, status=400
+                )
+
+            course_events = (
+                Event.objects.filter(course__id=course.id).order_by("date").all()
+            )
+            start_date, end_date = course_events.first().date, course_events.last().date
+            start_time = course_events.order_by("time_from").first().time_from
+            end_time = course_events.order_by("time_to").last().time_to
+
+            event_subset = (
+                Event.objects.exclude(id__in=[i.id for i in course_events])
+                .filter(date__gte=start_date, date__lte=end_date)
+                .exclude(Q(time_from__gte=end_time) | Q(time_to__lte=start_time))
+                .all()
+            )
+            events_grouped_by_date = {}
+
+            for i in event_subset:
+                if i.date not in events_grouped_by_date:
+                    events_grouped_by_date[i.date] = [i]
+                else:
+
+                    events_grouped_by_date[i.date].append(i)
+
+            colliding_events = [i for i in course_events]
+            for i in course_events:
+                if i.date not in events_grouped_by_date:
+                    continue
+                for j in events_grouped_by_date[i.date]:
+                    if not (i.time_from >= j.time_to or i.time_to <= j.time_from):
+                        colliding_events.append(j)
+
+            free_staffs = (
+                Staff.objects.exclude(
+                    staffs_events__event__id__in=[i.id for i in colliding_events]
+                )
+                .prefetch_related("staffs_events__event")
+                .all()
+            )
+            serialized_data = self.get_serializer(
+                self.paginate_queryset(free_staffs, request),
+                many=True,
+                expand=self.expand,
+                fields=self.fields,
+            )
+            return self.send_response(
+                False,
+                "all_good",
+                {**self.get_paginated_response(), "data": serialized_data.data},
+            )
 
         return self.send_response(
             True, "Available models are 'staff', 'event' or 'course'", {}, status=404
