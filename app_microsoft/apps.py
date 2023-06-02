c61504b4db10e4599834aed4c19a9a5ad1b73291
ninnroot <misterjames.thiha@gmail.com>
2023-06-02 19:39:58 +0800

    added a secret storage on s3 to download .pem file for ms connections

--- app_microsoft/apps.py
+++ app_microsoft/apps.py
@@ -1,6 +1,16 @@
+import boto3
+from decouple import config
 from django.apps import AppConfig
 
 
 class AppMicrosoftConfig(AppConfig):
     default_auto_field = "django.db.models.BigAutoField"
     name = "app_microsoft"
+
+    def ready(self):
+        s3 = boto3.client(
+            "s3",
+            aws_access_key_id=config("S3KEY"),
+            aws_secret_access_key=config("S3SECRET"),
+        )
+        s3.download_file("trsu-secrets", config("KEY"), f"./{config('KEY')}")
